03f06a88d20bc5d1b34e54bee15e3bc58ef1b85e
Kali Live user <henrywalkercoded@gmail.com>
2024-11-26 02:21:02 +0000

    new Snap

---
+++ test.js
@@ -0,0 +1,27 @@
+
+class Test extends Point  {
+    constructor(x, y) {
+        super(x, y)
+    }
+
+    corner(line1, line2, radius, style) {
+        // line = [vp, length, dir, r, style]
+
+        let f1 = this.line(line1[0], line1[1], line1[2], line1[3] || 0, 0, 0, 0, line1[4] || this.attri["line-style"])[1]
+        let f2 = this.line(line2[0], line2[1], line2[2], line2[3] || 0, 0, 0, 0, line2[4] || this.attri["line-style"])[1]
+
+
+        ctx.beginPath()
+        ctx.strokeStyle = style || this.attri["line-style"]
+        ctx.moveTo(f1.x, f1.y)
+        ctx.arcTo(this.x, this.y, f2.x, f2.y, 50)
+        ctx.stroke()
+        ctx.closePath()
+    }
+
+}
+
+let point = position(200, 1, 100, 1, 100, 1)
+let test = new Test(point.x, point.y)
+
+test.corner([vp.left_vp, 100, 1, 0], [vp.right_vp, 100, 1, 0])
